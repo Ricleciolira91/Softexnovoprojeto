1298c1d7fe4a087ad1046f5a7daab193bf44c827
Ricleciolira91 <ricleciolira@hotmail.com>
2025-06-25 22:56:22 -0300

    Primeiro commit do projeto Softex

---
+++ saudacao-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Saudacao from './Saudacao_softex'; // ou './Saudacao' se renomear
+
+function App() {
+  return (
+    <div className="App">
+      <Saudacao />
+    </div>
+  );
+}
+
+export default App;
---
+++ saudacao-app/src/Saudacao_softex.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Saudacao = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à aplicação front-end</h1>
+    </div>
+  );
+};
+
+export default Saudacao;
